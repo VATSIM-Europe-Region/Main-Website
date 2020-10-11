908fb23ae66db2d509f88e4e76a97a6ba0d99d97
Nikolas Goerlitz <nikki20488@gmail.com>
2020-10-11 13:58:14 +0200

    Update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,20 +8,20 @@
       </div>
       <div class="row mb-5 align-items-center">
         <div class="col-lg-7 mb-5" data-aos="fade-up" data-aos-delay="100">
-          <img src="images/undraw_youtube_tutorial.svg" alt="Image" class="img-fluid">
+          <img src="{{ asset('theme/images/dashboard-preview.png') }}" alt="Image" class="img-fluid">
         </div>
         <div class="col-lg-4 ml-auto" data-aos="fade-up" data-aos-delay="200">
-          <h2 class="text-black mb-4">We Are Excellent In Education</h2>
-          <p class="mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem maxime nam porro possimus fugiat quo molestiae illo.</p>
+          <h2 class="text-black mb-4">Everything available at a glance!</h2>
+          <p class="mb-4">The VATEMEA Dashboard allows you to manage everything from trainings to account settings.</p>
 
           <div class="d-flex align-items-center custom-icon-wrap mb-3">
-            <span class="custom-icon-inner mr-3"><span class="icon icon-graduation-cap"></span></span>
-            <div><h3 class="m-0">22,931 Yearly Graduates</h3></div>
+            <span class="custom-icon-inner mr-3"><span class="icon icon-cog"></span></span>
+            <div><h3 class="m-0">Manage your Account settings</h3></div>
           </div>
 
           <div class="d-flex align-items-center custom-icon-wrap">
-            <span class="custom-icon-inner mr-3"><span class="icon icon-university"></span></span>
-            <div><h3 class="m-0">150 Universities Worldwide</h3></div>
+            <span class="custom-icon-inner mr-3"><span class="icon icon-graduation-cap"></span></span>
+            <div><h3 class="m-0">Take theoretical examinations <br> and manage mentoring sessions</h3></div>
           </div>
 
         </div>
